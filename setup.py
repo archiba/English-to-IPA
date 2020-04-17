770788deac3f651ca51291b48161864903c5ca29
Yuki Chiba <yuki-chiba@outlook.jp>
2020-04-17 13:58:33 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='eng_to_ipa',
-    version='0.0.1',
+    version='0.0.2',
     description='take English text and convert it to IPA',
     author=['mphilli', 'Mitchellpkt', 'CanadianCommander', 'timvancann'],
     long_description=long_description,
